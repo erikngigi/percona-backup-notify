0e686ebcd20fe071adc754764486f2bebca52ec4
erikngigi <ericmosesngigi@gmail.com>
2025-09-11 18:13:36 +0300

    feat: send extra details to telegram

--- percona-backup.sh
+++ percona-backup.sh
@@ -33,11 +33,27 @@ get_last_full_backup
 create_incr_backup() {
 	# Generate a timestamp for the backups and log files
 	timestamp=$(date +%Y%m%d-%H%M%S)
+    target_dir="$INCR_BACKUP/backup-$timestamp"
 
-	xtrabackup --login-path=backup_operator --backup --target-dir="$INCR_BACKUP/backup-$timestamp" \
-		--incremental-basedir="$BACKUP/$LAST_FULL_BACKUP"
+	xtrabackup --login-path=backup_operator \
+        --backup \
+        --target-dir="$target_dir" \
+		--incremental-basedir="$BACKUP/$LAST_FULL_BACKUP" > /dev/null
 
-	send_message_telegram "Incremental backup completed."
+    # Extract info from xtrabackup_info
+    info_file="$target_dir/xtrabackup_info"
+
+    if [[ -f "$info_file" ]]; then
+        binlog_file=$(grep "binlog_pos" "$info_file" | awk '{print $3}')
+        binlog_pos=$(grep "binlog_pos" "$info_file" | awk '{print $4}')
+        gtid=$(grep "GTID of the last change" "$info_file" | cut -d= -f2 | xargs)
+
+        message="Incremental backup completed.\nDirectory: $target_dir\nBinlog: $binlog_file:$binlog_pos\nGTID: $gtid"
+    else
+        message="Incremental backup completed, but xtrabackup_info not found in $target_dir"
+    fi
+
+    send_message_telegram "$message"
 }
 
 create_incr_backup
